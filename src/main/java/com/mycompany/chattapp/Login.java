45695928ae68a25f13caf4ff9b00460678d7397d
Student <Student@LabVM2049939.xiulez5jvhfu1iulqpbsmedgvb.jnbx.internal.cloudapp.net>
2026-09-18 10:39:43 +0200

    i edited main app because it was giving me errors

---
+++ src/main/java/com/mycompany/chattapp/Login.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chattapp;
+
+/**
+ *
+ * @author Student
+ */
+class Login {
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+    public Login(String username, String password, String cellPhoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    public boolean checkUserName() {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity() {
+        boolean hasCapital = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+
+        if (password.length() < 8) {
+            return false;
+        }
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasCapital = true;
+            } else if (Character.isDigit(c)) {
+                hasNumber = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasCapital && hasNumber && hasSpecial;
+    }
+
+    public boolean checkCellPhoneNumber() {
+        return cellPhoneNumber.startsWith("+27") && cellPhoneNumber.length() == 12;
+    }
+    public String registerUser() {
+        if (!checkUserName()) {
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+
+        if (!checkPasswordComplexity()) {
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+
+        if (!checkCellPhoneNumber()) {
+            return "Cell phone number incorrectly formatted or does not contain an international code; please correct the number and try again.";
+        }
+
+        return "User has been registered successfully.";
+    }
+
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+        return username.equals(enteredUsername) && password.equals(enteredPassword);
+    }
+
+    public String returnLoginStatus(boolean loginStatus, String firstName, String lastName) {
+        if (loginStatus) {
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
+}
+   
