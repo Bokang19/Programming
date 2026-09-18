3d3fc1508e0374a6b3c703be9e75afa64c708e7f
Student <Student@LabVM2049939.xiulez5jvhfu1iulqpbsmedgvb.jnbx.internal.cloudapp.net>
2026-09-18 09:37:31 +0200

    initial commit

---
+++ src/main/java/com/mycompany/chattapp/ChattApp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chattapp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class ChattApp {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter South African cell number: ");
+        String cellPhoneNumber = input.nextLine();
+}
+
+    }
+
